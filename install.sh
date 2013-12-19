b9d703a8ec1e19e80cd0e7a4563314bccf1064c6
Tommy Ziegler <privat@tommyz.de>
2013-12-19 16:26:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,9 +22,8 @@ fi
 
 # TODO: Check if Git and Java is installed
 
-
+# Check and install libWiringPi
 WIRINGPI="/usr/local/lib/libwiringPi.so"
-
 if [ -f $WIRINGPI ];
 then
    echo "wiringPi is already installed [skip]"
@@ -40,9 +39,9 @@ else
    
 fi
 
+# Check and install RCSwitch-Pi
 RCSWITCH_REV="/opt/rcswitch-pi/sendRev"
 RCSWITCH_ELRO="/opt/rcswitch-pi/sendElro"
-
 if [[ -f $RCSWITCH_REV && -f $RCSWITCH_ELRO ]];
 then
    echo "RCSwitch-Pi is already installed [skip]"
@@ -59,8 +58,8 @@ else
    rm -rf rcswitch-pi
 fi
 
+# 5.) Check and install wHomeControl
 WHOMECONTROL_DIR="/opt/wHomeControl"
-
 if [ -d $WHOMECONTROL_DIR ];
 then
    echo "wHomeControl is already installed [skip]"
@@ -68,5 +67,20 @@ else
    echo "wHomeControl does not exist on Pi [install...]."
    
    mkdir -p $WHOMECONTROL_DIR
+   cd $WHOMECONTROL_DIR
+   git clone https://github.com/tommyziegler/home.pi.git
+   git clone https://github.com/tommyziegler/rcswitch-rest.git
 
+   # 5 a.) Check and install wHomeControl Deamon
+   WHOMECONTROLDEAMON="/etc/init.d/whomecontrol"
+   WHOMECONTROL_GITURL="https://raw.github.com/tommyziegler/wHomeControl-Installer/master/whomecontrol-deamon"
+   if [ -f $WHOMECONTROLDEAMON ];
+   then
+      echo " -> Deamon is already installed [skip]"
+   else
+      echo " -> Deamon does not exist on Pi [install...]."
+
+      curl -o $WHOMECONTROLDEAMON $WHOMECONTROL_GITURL
+      #mkdir -p $WHOMECONTROLDEAMON
+   fi
 fi
